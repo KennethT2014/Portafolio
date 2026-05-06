e20f8dff0bc53dede8191e88a37d83dbe081c706
KennethT2014 <kenneth.teller2014@gmail.com>
2026-05-06 02:02:22 -0600

    Feat Projects

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,26 +6,28 @@ const Hero = () => {
             <div className="col-span-3 flex flex-col justify-center">
                 <h1 className="text-hero text-heroTitle font-bold text-left p-4">Hola, soy</h1>
                 <h1 className="text-hero text-heroName font-bold text-left p-4">Kenneth Teller</h1>
-                <p className="text-subtitle text-heroDesc font-light text-left p-4">Product Manager & Software Developer</p>
+                <p className="text-subtitle text-heroDesc font-light text-left p-4">CEO <a href='https://www.sueltalo.me' className="text-blue-500 hover:text-blue-600 transition-colors">SUELTALO LLC</a></p>
                 <p className="text-body-responsive font-normal text-left p-4 text-heroGray">
-                    Apasionado por crear experiencias web excepcionales y soluciones innovadoras.
+                    Dedicado a la solución de problematicas reales mediante el uso de tecnologías.
                     <br />
-                    Especializado en desarrollo frontend y backend con tecnologías modernas.
+                    Especializado en soluciones innovadoras mediante tecnologías de vanguardia.
+                    <br />
+                    Lider de un equipo de desarrollo de software con experiencia en tecnologías web3, IA y desarrollo de software.
                 </p>
                 {/* Botones */}
                 <div className="flex gap-4 p-4">
                     <button className="text-body-responsive bg-white text-primary px-6 py-2 rounded-lg font-semibold shadow-soft hover:shadow-blue transition-all">
-                        My Projects
+                        <a href="#projects">My Projects</a>
                     </button>
                     <button className="text-body-responsive bg-transparent border-2 border-white text-primary px-6 py-2 rounded-lg font-semibold hover:bg-white/10 transition-colors">
-                        Contact me
+                        <a href="#contact">Contact me</a>
                     </button>
                 </div>
                 {/* Iconos */}
                 <div className="flex gap-4 p-4">
-                    <a href="" className=""></a>
-                    <a href="" className=""></a>
-                    <a href="" className=""></a>
+                    <a href="https://github.com/KennethT2014/KennethT2014" className="">Github</a>
+                    <a href="https://www.instagram.com/joan.teller2014/" className="">Instagram</a>
+                    <a href="https://www.linkedin.com/in/kenneth-teller-01476b278/" className="">LinkedIn</a>
                 </div>
             </div>
             <div className="col-span-1">
---
+++ src/components/Projects.tsx
@@ -0,0 +1,108 @@
+const Projects = () => {
+    return (
+        <section>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                <div className="col-span-1">
+                    <div className="bg-white rounded-lg shadow-soft p-4">
+                        <h2>GnomoStudios</h2>
+                        <p>
+                            Desarrollo de website para GnomoStudios
+                            <br />Estudio de diseño gráfico.
+                            <br />Diseño de marca.
+                            <br />Diseño UX/UI.
+                        </p>
+                        <button className="text-body-responsive bg-white text-primary px-6 py-2 rounded-lg font-semibold shadow-soft hover:shadow-blue transition-all">
+                            <a href='https://www.gnomostudios.com'>visitar</a>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <div className="bg-white rounded-lg shadow-soft p-4">
+                        <h2>Tecnomedia Shop</h2>
+                        <p>
+                            Desarrollo de website para Tecnomedia Shop
+                            <br />Tienda de productos tecnológicos.
+                        </p>
+                        <button className="text-body-responsive bg-white text-primary px-6 py-2 rounded-lg font-semibold shadow-soft hover:shadow-blue transition-all">
+                            <a href='https://www.tecnomediashop.es'>visitar</a>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <div className="bg-white rounded-lg shadow-soft p-4">
+                        <h2>Inversiones Valles</h2>
+                        <p>
+                            Desarrollo de landing page para Inversiones Valles
+                            <br />Inversiones Valles es una empresa dedicada a la<br />logistica y transporte de producciones agricolas.
+                        </p>
+                        <button className="text-body-responsive bg-white text-primary px-6 py-2 rounded-lg font-semibold shadow-soft hover:shadow-blue transition-all">
+                            <a href='https://www.inversionesvalles.com'>visitar</a>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <div className="bg-white rounded-lg shadow-soft p-4">
+                        <h2>TrustTrace</h2>
+                        <p>
+                            Desarrollo de website para TrustTrace
+                            <br />Plataforma de trazabilidad de productos.
+                        </p>
+                        <button className="text-body-responsive bg-white text-primary px-6 py-2 rounded-lg font-semibold shadow-soft hover:shadow-blue transition-all">
+                            <a href='https://www.trusttrace.es'>visitar</a>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <div className="bg-white rounded-lg shadow-soft p-4">
+                        <h2>AgroMaps</h2>
+                        <p>
+                            Desarrollo de website para AgroMaps
+                            <br />Plataforma de mapas para agricultura.
+                        </p>
+                        <button className="text-body-responsive bg-white text-primary px-6 py-2 rounded-lg font-semibold shadow-soft hover:shadow-blue transition-all">
+                            <a href='https://www.agromaps.es'>visitar</a>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <div className="bg-white rounded-lg shadow-soft p-4">
+                        <h2>Neoark Capital Fund</h2>
+                        <p>
+                            Desarrollo de sistema web3 + administrador web2 para Neoark Capital Fund
+                            <br />Plataforma de tokenizacion de activos.
+                        </p>
+                        <button className="text-body-responsive bg-white text-primary px-6 py-2 rounded-lg font-semibold shadow-soft hover:shadow-blue transition-all">
+                            <a href='https://www.neoarkcapital.io'>visitar</a>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <div className="bg-white rounded-lg shadow-soft p-4">
+                        <h2>Sueltalo</h2>
+                        <p>
+                            Desarrollo de Sueltalo
+                            <br />Infraestructura financiera web3.
+                            <br />Envio y recibo de activos financieros.
+                            <br />Wallet Multichain non-custodial.
+                        </p>
+                        <button className="text-body-responsive bg-white text-primary px-6 py-2 rounded-lg font-semibold shadow-soft hover:shadow-blue transition-all">
+                            <a href='https://www.sueltalo.me'>visitar</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
+
+
+
+
+
+
+
+
+
+
